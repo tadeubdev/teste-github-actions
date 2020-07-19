da2756c72a2a6d6462f19036b98c623f482ee119
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-07-19 18:04:03 -0300

    fe477aa

---
+++ precache-manifest.215ce68144026e7a251db0eb9bdd7d8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb52f4641aebf804d88c",
+    "url": "/teste-github-actions/css/app.0e88525e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/teste-github-actions/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4a3fc205f4f5e7f39cce621e0e1c21e4",
+    "url": "/teste-github-actions/index.html"
+  },
+  {
+    "revision": "36b0760f8aed67759d35",
+    "url": "/teste-github-actions/js/about.e3e43162.js"
+  },
+  {
+    "revision": "eb52f4641aebf804d88c",
+    "url": "/teste-github-actions/js/app.31c05bff.js"
+  },
+  {
+    "revision": "1749a7d14aa29e251baa",
+    "url": "/teste-github-actions/js/chunk-vendors.c97a7ce9.js"
+  },
+  {
+    "revision": "7d0fac9dc7844b6571ec88c8ba0e37c8",
+    "url": "/teste-github-actions/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/teste-github-actions/robots.txt"
+  }
+]);
